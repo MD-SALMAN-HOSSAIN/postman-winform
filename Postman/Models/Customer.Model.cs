db89fb29e35bd6efe835314b2919427926d3a03c
Hasan Ahamed <officials.hasan@gmail.com>
2021-12-13 19:09:29 +0600

    customer repo added

---
+++ Postman/Models/Customer.Model.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Postman.Models
+{
+    public class Customer
+    {
+        public string name { get; set; }
+
+        public string phone { get; set; }
+
+
+        public string address { get; set; }
+
+        public string area { get; set; }
+        
+        public string city { get; set; }
+
+    }
+}
