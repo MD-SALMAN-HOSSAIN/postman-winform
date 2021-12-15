d2a6bf4bfdd91d8bd26f12a0ef345a0468b3aaeb
Hasan Ahamed <officials.hasan@gmail.com>
2021-12-15 11:42:39 +0600

    customer table added

--- Postman/App/Merchent/CustomerTableForm.cs
+++ Postman/App/Merchent/CustomerTableForm.cs
@@ -7,16 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Postman.Repository;
+using Postman.Models;
 namespace Postman.App.Merchent
 {
     public partial class CustomerTableForm : Form
     {
-
+        CustomerRepostiroy customer = new CustomerRepostiroy();
         public CustomerTableForm()
         {
             InitializeComponent();
-            this.guna2DataGridView1 = 
+            this.guna2DataGridView1.DataSource = customer.getAllCustomer();
+        }
+        public CustomerTableForm(User user)
+        {
+            InitializeComponent();
+            this.guna2DataGridView1.DataSource = customer.getOneCustomer(user.id);
         }
     }
 }
